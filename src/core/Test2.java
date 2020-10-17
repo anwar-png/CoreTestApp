e0044ab032bd04e8bb46784b9a8806b468c8efb3
Global <Global@Global-PC>
2020-10-17 12:26:52 +0530

    second commit

--- src/core/Test2.java
+++ src/core/Test2.java
@@ -3,100 +3,39 @@ package core;
 public class Test2 {
 
 	public static void main(String[] args) {
-		
-		int i=1000;  //int is of 4 bytes
-		
-		byte b=(byte)i;  //byte is of 1 byte so we cannot assign higher data type 
-		
-		           //into smaller data type
-		
-		  //type casting the one data type into another data type 
-		
-		System.out.println("int type"+i);
-		
-		System.out.println("byte type"+b);
-		
-		//converting the one data type into another datatype is called
-		
-		//type casting
-		
-		//here we are only converting the datatype not the value
-		
-		
-		char ch=65;
-		
-		int x=(int)ch;
-		
-		System.out.println(x);
-		
-		System.out.println(ch);
-		
-		
-		 double d=13.333333333;
-		 
-		 float f=(float)d;
-		 
-		 System.out.println("double Value:"+"  "+d);
-		 
-		 System.out.println("float value:"+"  "+f);
-		 
-		 
-		 //primitive types
-		 
-		 //non primitive
-		 
-		 
-		  
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		 
-		   
-		  
-		  
-		  
-		  
-		  
-		  
-		  
-		
-		
-		
-		
-		
-		
-		
 
+		int i = 1000;
+
+		byte b = (byte) i;
+
+		System.out.println("int type" + i);
+
+		System.out.println("byte type" + b);
+
+		char ch = 65;
+
+		int x = (int) ch;
+
+		System.out.println(x);
+
+		System.out.println(ch);
+
+		double d = 13.333333333;
+
+		float f = (float) d;
+
+		System.out.println("double Value:" + "  " + d);
+
+		System.out.println("float value:" + "  " + f);
+
+		// primitive types
+
+		// non primitive
+		
+		System.out.println("=================================");
+		System.out.println("=================================");
+
+		
 	}
 
 }
